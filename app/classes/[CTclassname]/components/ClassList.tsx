3a7e1486aeb7dd585be5d8e2276086ca22d47b93
adamsaunders26 <a.saunders206@gmail.com>
2023-09-14 11:49:35 +0100

    when clicked, children will update currentPupil state

--- app/classes/[CTclassname]/components/ClassList.tsx
+++ app/classes/[CTclassname]/components/ClassList.tsx
@@ -1,15 +1,24 @@
+import Link from "next/link";
+import { Dispatch, SetStateAction } from "react";
+
 interface Props {
   CTclass: CTClass | null;
+  setCurrentPupil: Dispatch<SetStateAction<Pupil | null>>;
 }
 
-export default function ClassList({ CTclass }: Props) {
+export default function ClassList({ CTclass, setCurrentPupil }: Props) {
   if (CTclass) {
     return (
       <section className="border-4 p-4 border-blue-500">
         <ul>
           <h2>Name:</h2>
           {CTclass.pupils.map((pupil) => (
-            <li>{`${pupil.first_name} ${pupil.last_name}`}</li>
+            <li
+              key={pupil.id}
+              onClick={() => {
+                setCurrentPupil(pupil);
+              }}
+            >{`${pupil.first_name} ${pupil.last_name}`}</li>
           ))}
         </ul>
       </section>
