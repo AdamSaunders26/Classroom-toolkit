36f756129d74f6b64a960e24fcc46fb00c343cc3
adamsaunders26 <a.saunders206@gmail.com>
2023-09-14 11:50:44 +0100

    shows pupil details

--- app/classes/[CTclassname]/components/PupilDetails.tsx
+++ app/classes/[CTclassname]/components/PupilDetails.tsx
@@ -1,12 +1,21 @@
-export default function PupilDetails() {
+interface Props {
+  pupil: Pupil | null;
+}
+
+export default function PupilDetails({ pupil }: Props) {
   return (
-    <section className="border-4 border-pink-500">
-      <h2>Class Name - Year X</h2>
-      <div className=" p-4">
-        <p>Name</p>
-        <p>Age</p>
-        <p>Top maths set</p>
-        <p>No headteachers award</p>
+    <section className="border-4 border-pink-500 p-4">
+      <h2>
+        {pupil?.CTclass} - Year {pupil?.year}
+      </h2>
+      <div className="">
+        <p>
+          Name: {pupil?.first_name} {pupil?.last_name}
+        </p>
+        <p>
+          Other things will go here once implemented, such as sections of
+          reward/homework trackers
+        </p>
       </div>
     </section>
   );
