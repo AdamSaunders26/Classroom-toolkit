1e1595d386f249b47a66d5a476a0fe1eb301698f
adamsaunders26 <a.saunders206@gmail.com>
2023-11-09 22:33:31 +0000

    fix type error

--- app/classes/components/LoadingSpinner.tsx
+++ app/classes/components/LoadingSpinner.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import CTLogo from "../../icon.svg";
 
-export default function LoadingSpinner(h = 8, w = 8) {
+export default function LoadingSpinner() {
   return (
     <div className="text-xl p-4 place-self-center col-span-4 animate-spin-slow">
       <Image
